2a4a893fbfb1a0815021448eff7239334ee4ddf8
Abhinav Pandey <abhinavpandey1230@gmail.com>
2024-12-14 20:16:43 +0530

    Update user model to include MongoDB fields and add MongoDB driver dependency
    - Added MongoDB driver dependency (go.mongodb.org/mongo-driver v1.17.1) to go.mod and go.sum.
    - Enhanced User model in user.go to include fields for MongoDB integration, such as ID, Username, Name, Password, and various API keys.
    
    Signed-off-by: Abhinav Pandey <abhinavpandey1230@gmail.com>

--- server/models/user.go
+++ server/models/user.go
@@ -1,3 +1,15 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 // User Model
+type User struct {
+	ID             primitive.ObjectID `bson:"_id,omitempty"`
+	Username       string             `bson:"username"`
+	Name           string             `bson:"name"`
+	Password       string             `bson:"password"`
+	YTApiKey       string             `bson:"yt_api_key"`
+	GeminiApiKey   string             `bson:"gemini_api_key"`
+	OpenAIApiKey   string             `bson:"openai_api_key"`
+	ClaudeApiKey   string             `bson:"claude_api_key"`
+}
